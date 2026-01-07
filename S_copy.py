54f63987fea351bf66df394a456beca5ebdfac56
Taozaz1987 <157225038+Taozaz1987@users.noreply.github.com>
2026-01-07 21:29:05 +0800

    Add SEGY headers for WGstack export

--- S_copy.py
+++ S_copy.py
@@ -28,6 +28,7 @@
 # from myprog import *
 #
 from rsf.prog import *
+import rsf.api as rsf
 
 
 ###########################################################################################
@@ -53,6 +54,62 @@ def result5 (data, other):
 
 Flow ('wgstack', './WGstack.H', 'dd form=native')
 result ('wgstack', 'screenratio=1 label2="Distance(mile)" ')
+
+wgstack_in = rsf.Input('WGstack.H')
+wgstack_n2 = wgstack_in.int('n2')
+wgstack_o2 = wgstack_in.float('o2', 0.0)
+wgstack_d2 = wgstack_in.float('d2', 1.0)
+wgstack_in.close()
+
+# Coordinate unit scaling (set to 1.0 for same units as o2/d2; adjust if needed)
+coord_scale = 1.0
+
+Flow(
+    'wgstack_tracenum',
+    None,
+    'spike n1=1 n2=%d | put d2=1 o2=1 | math output="x2" | dd type=int'
+    % wgstack_n2,
+)
+Flow(
+    'wgstack_sx',
+    None,
+    'spike n1=1 n2=%d | put d2=%g o2=%g | math output="x2*%g" | dd type=int'
+    % (wgstack_n2, wgstack_d2, wgstack_o2, coord_scale),
+)
+Flow('wgstack_gx', 'wgstack_sx', 'dd type=int')
+Flow(
+    'wgstack_cdp',
+    None,
+    'spike n1=1 n2=%d | put d2=%g o2=%g | math output="(x2-%g)/%g+1" | dd type=int'
+    % (wgstack_n2, wgstack_d2, wgstack_o2, wgstack_o2, wgstack_d2),
+)
+Flow(
+    'wgstack_offsets',
+    None,
+    'spike n1=1 n2=%d | math output="0" | dd type=int'
+    % wgstack_n2,
+)
+Flow(
+    'wgstack_headers',
+    [
+        'wgstack_tracenum',
+        'wgstack_sx',
+        'wgstack_gx',
+        'wgstack_cdp',
+        'wgstack_offsets',
+    ],
+    '''
+    segyheader
+    tracl=${SOURCES[0]}
+    tracf=${SOURCES[0]}
+    sx=${SOURCES[1]}
+    gx=${SOURCES[2]}
+    cdp=${SOURCES[3]}
+    offset=${SOURCES[4]}
+    ''',
+)
+
+Flow('wgstack.segy', ['wgstack', 'wgstack_headers'], 'segywrite tfile=${SOURCES[1]}')
 
 
 #
